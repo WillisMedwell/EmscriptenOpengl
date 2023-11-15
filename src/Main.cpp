4d9ec7a8c358ba16324a1073e099c905918bb094
WillisMedwell <medwellwillis@gmail.com>
2023-11-15 22:49:43 +1100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,25 +1,27 @@
 #include <array>
+#include <filesystem>
 #include <glm/glm.hpp>
 #include <iostream>
-#include <filesystem>
-
 
 #include "Application.hpp"
+#include "Ecs.hpp"
 #include "Loader.hpp"
 #include "renderer/3d/Scene.hpp"
-#include "Ecs.hpp"
+
 
 #include <glaze/glaze.hpp>
 
-
-
-
 namespace fs = std::filesystem;
 
-void print_directory(const fs::path& path) {
+void print_directory(const fs::path& path)
+{
     if (fs::exists(path) && fs::is_directory(path)) {
-        for (const auto& entry : fs::recursive_directory_iterator(path)) {
-            std::cout << entry.path() << std::endl;
+        try {
+            for (const auto entry : fs::recursive_directory_iterator(path)) {
+                std::cout << entry.path() << std::endl;
+            }
+        } catch (std::exception& e) {
+            
         }
     } else {
         std::cerr << "Provided path is not a directory." << std::endl;
@@ -32,11 +34,11 @@ auto printAndQuit = [](auto msg) -> Expected<void, std::string_view> {
     return {};
 };
 
-int main() {
+int main()
+{
 
-    fs::path current_path = fs::current_path();    
+    fs::path current_path = fs::current_path();
     print_directory(current_path);
-
 
     Application app;
 
